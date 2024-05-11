9ed345c0a476d691a59ae226fd6062dac56c610d
minusno0708 <naoyao1014@gmail.com>
2024-05-11 16:01:28 +0900

    feat:cowboyで作成したサーバを起動

--- src/cowboy_todo_app.erl
+++ src/cowboy_todo_app.erl
@@ -10,6 +10,16 @@
 -export([start/2, stop/1]).
 
 start(_StartType, _StartArgs) ->
+    Dispatch = cowboy_router:compile([
+        {<<"localhost">>, [
+            {<<"/">>, hello_handler, []}
+        ]}
+    ]),
+    {ok, _} = cowboy:start_clear(
+        http,
+        [{port, 8080}],
+        #{env => #{dispatch => Dispatch}}
+    ),
     cowboy_todo_sup:start_link().
 
 stop(_State) ->
